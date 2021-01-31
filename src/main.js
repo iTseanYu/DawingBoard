218342fae1166a35bece80707b3aa862c9bb2491
seanyu <itseanyu@163.com>
2021-01-31 14:00:35 +0800

    调试

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
+console.log(123)
 new Vue({
   el: '#container',
   router,
