0c11931dd63c428f8a9591c660465b9e565c0e58
seanyu <itseanyu@163.com>
2021-01-31 14:18:32 +0800

    test

--- src/main.js
+++ src/main.js
@@ -2,17 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import container from './container'
+import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 /* eslint-disable no-new */
 console.log(123)
+// new Vue({
+//   el: '#container',
+//   router,
+//   components: { container },
+//   template: '<container/>'
+// })
 new Vue({
-  el: '#container',
+  el: '#App',
   router,
-  components: { container },
-  template: '<container/>'
+  components: { App },
+  template: '<App/>'
 })
