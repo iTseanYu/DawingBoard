441bfc082f299bbddee5e14821372929ffa0e185
iTseanYu <38932234+iTseanYu@users.noreply.github.com>
2021-01-31 13:34:09 +0800

    Revert "1"

---
+++ src/components/exement.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="XXX">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XXXX',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
